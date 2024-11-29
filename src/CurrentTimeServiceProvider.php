28e2a316b7b4124857e3066a510324b081d7312b
Juan Pablo Aqueveque <jp.aqueveque@gmail.com>
2024-11-29 16:13:26 -0300

    remove tabs

--- src/CurrentTimeServiceProvider.php
+++ src/CurrentTimeServiceProvider.php
@@ -6,9 +6,9 @@ use Illuminate\Support\ServiceProvider;
 
 class CurrentTimeServiceProvider extends ServiceProvider
 {
-	public function boot()
-	{
-		$this->loadRoutesFrom(__DIR__.'/routes/web.php');
-		$this->loadViewsFrom(__DIR__.'/resources/views', 'current-time');
-	}
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+        $this->loadViewsFrom(__DIR__.'/resources/views', 'current-time');
+    }
 }
--- src/Http/Controllers/TimeController.php
+++ src/Http/Controllers/TimeController.php
@@ -6,10 +6,10 @@ use Illuminate\Http\Request;
 
 class TimeController
 {
-  public function show()
-  {
-    return view('current-time::time', [
-      'time' => now()->format('Y-m-d H:i:s')
-    ]);
-  }
+    public function show()
+    {
+        return view('current-time::time', [
+          'time' => now()->format('Y-m-d H:i:s')
+        ]);
+    }
 }
